69f451954c5898d94b00cd34d3cb065355631b41
Gabriel C Moraru <gabrielcmoraru@gmail.com>
2018-08-05 13:46:07 +0100

    feat: added map component(after trying 7 diffrenet libs); style for map as well;

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Map from './Map';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Home</h1>
+        <Map />
       </div>
     );
   }
--- src/Map.jsx
+++ src/Map.jsx
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import mapStyle from './mapStyle.json';
 
 
-export default class Map extends Component {
+class Map extends Component {
+  static defaultProps = {
+    center: {
+      lat: 51.4276472,
+      lng: -0.1701949,
+    },
+    zoom: 16,
+  };
+
   render() {
     return (
-      <div>
-        <h1>
-Map
-</h1>
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          options={{ styles: mapStyle }}
+          bootstrapURLKeys={{ key: 'AIzaSyDuVUBfjdAwk8mVwkhVZl794692wvszwi8' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        />
       </div>
     );
   }
 }
+
+export default Map;
