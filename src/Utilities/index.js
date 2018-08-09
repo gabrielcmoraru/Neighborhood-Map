805b985c9afc4551d67216390536bcbeddbd051b
Gabriel C Moraru <gabrielcmoraru@gmail.com>
2018-08-09 06:52:24 +0100

    feat: added utilities (colors, animation, shadows, toggle)

---
+++ src/Utilities/index.js
@@ -0,0 +1,11 @@
+import Toggle from './ToggleInfo';
+import elevation from './elevation';
+import transition from './transition';
+import colors from './colors';
+
+export * from './colors';
+export * from './position';
+
+export {
+  Toggle, elevation, transition, colors,
+};
