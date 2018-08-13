b9e37911e6665876521e96a0745fef4d6a50dfa2
Gabriel C Moraru <gabrielcmoraru@gmail.com>
2018-08-13 16:59:18 +0100

    chore: added to readme

--- src/keys.js
+++ src/keys.js
@@ -1,7 +1,7 @@
 
-
+/* Google API */
 export const gApiKey = 'AIzaSyDuVUBfjdAwk8mVwkhVZl794692wvszwi8';
-
+/* Fousquare API ClientId */
 export const fsClientId = 'MDL3L4ZAR3BCRYQGBLUNCH5FW1CYY43XJQCCIAAJFH1HM21M';
-
+/* Fousquare API ClientSecret */
 export const fsClientSecret = 'XKLCEBLVI125M2WMHTJN3JGH33EPBMI5FGV2F3MPBJBPTRLY';
